310c5dcf4d523dfa8bdfd886941bd5959e63f58e
Elian-beep <elianoliver101@gmail.com>
2023-03-23 22:56:30 -0300

    all endpoints of Devolutiva

--- src/main/java/uea/cautela_drive/resources/DevolutivaResource.java
+++ src/main/java/uea/cautela_drive/resources/DevolutivaResource.java
@@ -6,12 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uea.cautela_drive.dto.ResumoDevolutivaDto;
 import uea.cautela_drive.models.Devolutiva;
+import uea.cautela_drive.models.Requisicao;
 import uea.cautela_drive.repositories.filters.DevolutivaFilter;
 import uea.cautela_drive.services.DevolutivaService;
 
@@ -24,6 +30,12 @@ public class DevolutivaResource {
 	@Autowired
 	private DevolutivaService devolutivaService;
 	
+	@PostMapping
+	public ResponseEntity<Devolutiva> criar(@RequestBody Devolutiva devolutiva){
+		Devolutiva devolutivaSalva = devolutivaService.criar(devolutiva);
+		return ResponseEntity.ok().body(devolutivaSalva);
+	}
+	
 	@GetMapping(value = "/busca")
 	public ResponseEntity<Page<ResumoDevolutivaDto>> resumir(DevolutivaFilter devolutivaFilter, Pageable pageable){
 		Page<ResumoDevolutivaDto> resumos = devolutivaService.resumir(devolutivaFilter, pageable);
@@ -34,6 +46,24 @@ public class DevolutivaResource {
 	public ResponseEntity<List<Devolutiva>> listar(){
 		List<Devolutiva> devolutivas = devolutivaService.listar();
 		return ResponseEntity.ok().body(devolutivas);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Devolutiva> buscarPorCodigo(@PathVariable Long id){
+		Devolutiva devolutiva = devolutivaService.buscarPorId(id);
+		return ResponseEntity.ok().body(devolutiva);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id){
+		devolutivaService.excluir(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Devolutiva> atualizar(@PathVariable Long id, @RequestBody Devolutiva devolutiva){
+		Devolutiva devolutivaSalva = devolutivaService.atualizar(id, devolutiva);
+		return ResponseEntity.ok().body(devolutivaSalva);
 	}
 	
 }
--- src/main/java/uea/cautela_drive/services/DevolutivaService.java
+++ src/main/java/uea/cautela_drive/services/DevolutivaService.java
@@ -2,13 +2,21 @@ package uea.cautela_drive.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import uea.cautela_drive.dto.ResumoDevolutivaDto;
+import uea.cautela_drive.models.Coordenador;
 import uea.cautela_drive.models.Devolutiva;
+import uea.cautela_drive.models.Motorista;
+import uea.cautela_drive.models.Requisicao;
+import uea.cautela_drive.models.Veiculo;
 import uea.cautela_drive.models.enums.Situacao;
 import uea.cautela_drive.repositories.DevolutivaRepository;
 import uea.cautela_drive.repositories.MotoristaRepository;
@@ -26,15 +34,48 @@ public class DevolutivaService {
 	private MotoristaRepository motoristaRepository;
 	@Autowired
 	private VeiculoRepository veiculoRepository;
-	
-	public Page<ResumoDevolutivaDto> resumir(DevolutivaFilter devolutivaFilter, Pageable pageable){
-		if(devolutivaFilter.getSituacao().equals(Situacao.AUTORIZADO)) {			
+
+	public Devolutiva criar(Devolutiva devolutiva) {
+		Requisicao requisicao = requisicaoRepository.findById(devolutiva.getRequisicao().getId()).orElseThrow();
+
+		if (devolutiva.getSituacao().equals(Situacao.AUTORIZADO)) {
+			Motorista motorista = motoristaRepository.findById(devolutiva.getMotorista().getId()).orElseThrow();
+			Veiculo veiculo = veiculoRepository.findById(devolutiva.getVeiculo().getId()).orElseThrow();
+			return devolutivaRepository.save(devolutiva);
+		}
+		return devolutivaRepository.save(devolutiva);
+	}
+
+	public Page<ResumoDevolutivaDto> resumir(DevolutivaFilter devolutivaFilter, Pageable pageable) {
+		if (devolutivaFilter.getSituacao().equals(Situacao.AUTORIZADO)) {
 			return devolutivaRepository.filtrarAutorizado(devolutivaFilter, pageable);
 		}
 		return devolutivaRepository.filtrar(devolutivaFilter, pageable);
 	}
-	
+
 	public List<Devolutiva> listar() {
 		return devolutivaRepository.findAll();
+	}
+
+	public Devolutiva buscarPorId(Long id) {
+		Devolutiva devolutiva = devolutivaRepository.findById(id).orElseThrow();
+		return devolutiva;
+	}
+
+	public void excluir(Long id) {
+		devolutivaRepository.deleteById(id);
+	}
+
+	public Devolutiva atualizar(Long id, Devolutiva devolutiva) {
+		Devolutiva devolutivaSalva = devolutivaRepository.findById(id).orElseThrow();
+		Requisicao requisicao = requisicaoRepository.findById(devolutiva.getRequisicao().getId()).orElseThrow();
+
+		if (devolutiva.getSituacao().equals(Situacao.AUTORIZADO)) {
+			Motorista motorista = motoristaRepository.findById(devolutiva.getMotorista().getId()).orElseThrow();
+			Veiculo veiculo = veiculoRepository.findById(devolutiva.getVeiculo().getId()).orElseThrow();
+			return devolutivaRepository.save(devolutiva);
+		}
+		BeanUtils.copyProperties(devolutiva, devolutivaSalva, "id");
+		return devolutivaRepository.save(devolutivaSalva);
 	}
 }
